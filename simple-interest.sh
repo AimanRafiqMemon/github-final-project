f48e5e2b858c244f88daf50e0f6deb46b4bd23fc
AimanRafiqMemon <92150895+AimanRafiqMemon@users.noreply.github.com>
2022-04-30 09:34:52 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
+    This script calculates simple interest given principal,
+    annual rate of interest and time period in years.
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+    Author: Upkar Lidder (IBM)
+    Additional Authors:
+    AimanRafiqMemon
 
-   # Input:
-   # p, principal amount
-   # t, time period in years
-   # r, annual rate of interest
+    Input:
+    p, principal amount
+    t, time period in years
+    r, annual rate of interest
 
    # Output:
    # simple interest = p*t*r
